095c73be7a5a53451c64de25b1c0f5d889149b0d
Chris <camera@viabox>
2016-08-05 10:22:24 -0600

    Create mp4 files instead of mov

--- createvideo.sh
+++ createvideo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PIX_HOME=/home/camera/pics
+VIDEO=/home/camera/video
 
 if [ $# -eq 0 ] 
 then
@@ -15,7 +16,12 @@ DIR="$PIX_HOME/$DATE"
 cd $DIR
 pwd
 
-ffmpeg -r 10 -pattern_type glob -i 'frontdoor_*.jpg' -vcodec mjpeg -n frontdoor_$DATE.mov
-ffmpeg -r 10 -pattern_type glob -i 'frontwindow_*.jpg' -vcodec mjpeg -n frontwindow_$DATE.mov
+# ffmpeg -r 10 -pattern_type glob -i 'frontdoor_*.jpg' -vcodec mjpeg -n frontdoor_$DATE.mov
+# ffmpeg -r 10 -pattern_type glob -i 'frontwindow_*.jpg' -vcodec mjpeg -n frontwindow_$DATE.mov
+
+ffmpeg -r 10 -pattern_type glob -i 'frontdoor_*.jpg' -c:v libx264 -pix_fmt yuv420p -profile:v high -level 4.2 -crf 25 $VIDEO/frontdoor_$DATE.mp4
+ffmpeg -r 10 -pattern_type glob -i 'frontwindow_*.jpg' -c:v libx264 -pix_fmt yuv420p -profile:v high -level 4.2 -crf 25 $VIDEO/frontwindow_$DATE.mp4  
+
 
 echo "$(date): createvideo executed" >> createvideo.log
+# ffmpeg -i frontwindow_20160804.mov -c:v libx264 -pix_fmt yuv420p -profile:v high -level 4.2 -crf 25 frontwindow_20160804.mp4
